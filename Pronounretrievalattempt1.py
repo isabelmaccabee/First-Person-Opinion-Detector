83072cb25892218042d31a182ab63f877d923a2e
isabelmaccabee <isabel.maccabee@gmail.com>
2016-06-01 15:21:48 +0100

    Word and pronoun count

--- Pronounretrievalattempt1.py
+++ Pronounretrievalattempt1.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
-file = open('Examples/Guardian.txt')
-source = file.read()
-pronouns = ["i", "my", "me", "mine", "we", "us", "our", "ours"] 
+import os
 
-count = 0
-words = source.split ()
-for word in words:
-    if word.lower() in pronouns:
-    	count += 1
-    	# print word
+folders = ["Non-Opinion", "Opinion"]
+for folder in folders:
+	print ""
+	print folder
 
-print count
+	filenames = os.listdir('Examples/' + folder)
+	for filename in filenames:
+
+		file = open('Examples/' + folder + "/" + filename)
+		source = file.read()
+		pronouns = ["i", "my", "me", "mine", "we", "us", "our", "ours"] 
+
+		count = 0
+		words = source.split ()
+		for word in words:
+		    if word.lower() in pronouns:
+		    	count += 1
+		    	# print word
+
+		pronounperword = float(count/float(len(words)))
+
+		print str(count) + " " + str(format(pronounperword, '.2f')) + " " + filename
