c2ef300b78f05debbd453b273ce4bcf35018ce90
isabelmaccabee <isabel.maccabee@gmail.com>
2016-06-03 16:16:46 +0100

    Save text body as file w ID name

--- gettext.py
+++ gettext.py
@@ -7,5 +7,19 @@ body = r1.read()
 # print body
 import json
 bodyJson = json.loads(body)
-print bodyJson['id']
-print bodyJson['metadata']['http://schema.org/text']
+article_id = bodyJson['id']
+article_text = bodyJson['metadata']['http://schema.org/text']
+# import os
+# for os import rename, listdir
+# # 	# os.rename(bodyJson['metadata']['http://schema.org/text'], bodyJson['id'])
+# article_text
+# f = open(article_text, 'w')
+# f.write(article_text.encode('utf8'))
+# f.close()
+
+file = open(article_id + ".txt", 'w')
+file.write(article_text.encode('utf8'))
+file.close
+# file = open(article_id + ".txt", 'w')
+# file.write(article_text)
+# file.close()
